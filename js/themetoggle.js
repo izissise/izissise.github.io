6f6b563c7bc39d82725e78ea9639b149c55faeee
Hugues Morisset <morisset.hugues@gmail.com>
2024-02-10 19:08:14 +0100

    Publish 2024-02-10

--- js/themetoggle.js
+++ js/themetoggle.js
@@ -1,1 +1,41 @@
-function setTheme(e){localStorage.setItem("theme-storage",e);let t=document.querySelector("html");e==="dark"?(document.getElementById("darkModeStyle").disabled=!1,t.classList.remove("light"),t.classList.add("dark"),document.getElementById("dark-mode-toggle").children[0].style=""):e==="light"&&(document.getElementById("darkModeStyle").disabled=!0,t.classList.remove("dark"),t.classList.add("light"),document.getElementById("dark-mode-toggle").children[0].style="filter: invert(1);")}function toggleTheme(){localStorage.getItem("theme-storage")==="light"?setTheme("dark"):localStorage.getItem("theme-storage")==="dark"&&setTheme("light")}function getSavedTheme(){let e=localStorage.getItem("theme-storage");return e||(window.matchMedia&&window.matchMedia("(prefers-color-scheme: dark)").matches?e="dark":e="light"),e}setTheme(getSavedTheme())
+function setTheme(mode) {
+    localStorage.setItem("theme-storage", mode);
+    let htmlElement = document.querySelector("html");
+
+    if (mode === "dark") {
+        document.getElementById("darkModeStyle").disabled = false;
+        htmlElement.classList.remove("light")
+        htmlElement.classList.add("dark")
+
+        document.getElementById("dark-mode-toggle").children[0].style = "";
+    } else if (mode === "light") {
+        document.getElementById("darkModeStyle").disabled = true;
+        htmlElement.classList.remove("dark")
+        htmlElement.classList.add("light")
+
+        document.getElementById("dark-mode-toggle").children[0].style = "filter: invert(1);";
+    }
+}
+
+function toggleTheme() {
+    if (localStorage.getItem("theme-storage") === "light") {
+        setTheme("dark");
+    } else if (localStorage.getItem("theme-storage") === "dark") {
+        setTheme("light");
+    }
+}
+
+function getSavedTheme() {
+    let currentTheme = localStorage.getItem("theme-storage");
+    if(!currentTheme) {
+        if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+            currentTheme = "dark";
+        } else {
+            currentTheme = "light";
+        }
+    }
+
+    return currentTheme;
+}
+
+setTheme(getSavedTheme());
